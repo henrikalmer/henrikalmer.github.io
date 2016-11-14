a2e24a7d3f07be490d7f5c43d62fcf2ce348ce38
Henrik Almér <henrik.almer@booli.se>
2016-11-14 16:30:03 +0100

    Update heatmap with new layers

--- js/map.js
+++ js/map.js
@@ -1,48 +1,39 @@
 // State
 var state = {
-  'perspective': 'extruded',
+  'perspective': 'flat',
   'selection': 'combined',
 };
-
-// Booli API Auth
-var auth = {};
-auth.callerId = "bopriskarta";
-auth.time = Math.round(Date.now() / 1000);
-auth.unique = Math.random().toString(8).slice(0, 15)
-var shasum = new jsSHA("SHA-1", "TEXT");
-shasum.update(auth.callerId + auth.time + "1KpJKCMVFzZ2s2YqEv3CPqTMUmzl76V5mwNiQw1a" + auth.unique);
-auth.hash = shasum.getHash("HEX");
 
 // Define height and color stops
 var sqmprices = _.range(0, 115000, 5000)
 var sqmprice_height_stops = sqmprices.map(function (e, i) {
   return [e, e/10];
 });
-var prices = _.range(0, 14000000, 250000)
+var prices = _.range(0, 22250000, 250000)
 var price_height_stops = prices.map(function (e, i) {
   return [e, e/1000];
 });
 var sqmprice_color_stops = [
-  [9236, "#4575b4"],
-  [21265, "#74add1"],
-  [27455, "#abd9e9"],
-  [32628, "#e0f3f8"],
-  [38891, "#ffffbf"],
-  [47135, "#fee090"],
-  [59069, "#fdae61"],
-  [79984, "#f46d43"],
-  [113000, "#d73027"],
+  [8653, "#4575b4"],
+  [19058, "#74add1"],
+  [24814, "#abd9e9"],
+  [28888, "#e0f3f8"],
+  [33997, "#ffffbf"],
+  [42132, "#fee090"],
+  [54107, "#fdae61"],
+  [76227, "#f46d43"],
+  [113021, "#d73027"],
 ];
 var price_color_stops = [
-  [1188684, "#4575b4"],
-  [2738977, "#74add1"],
-  [3402967, "#abd9e9"],
-  [4000397, "#e0f3f8"],
-  [4725219, "#ffffbf"],
-  [5715213, "#fee090"],
-  [7025697, "#fdae61"],
-  [9020642, "#f46d43"],
-  [13584587, "#d73027"],
+  [923806, "#4575b4"],
+  [2700251, "#74add1"],
+  [3478935, "#abd9e9"],
+  [4244821, "#e0f3f8"],
+  [5298372, "#ffffbf"],
+  [6867953, "#fee090"],
+  [9102199, "#fdae61"],
+  [12605582, "#f46d43"],
+  [22102526, "#d73027"],
 ];
 
 // Define map layers
@@ -88,7 +79,7 @@ var layers = {
 
 // Flat layers
 layers['apt']['id'] = 'apt-polygons';
-layers['apt']['source-layer'] = 'sthlm-lan-sqmprice-polygons-60k';
+layers['apt']['source-layer'] = 'apts-sqmprice-polygons-final';
 layers['apt']['paint']['fill-color']['stops'] = sqmprice_color_stops;
 layers['apt-clicked'] = JSON.parse(JSON.stringify(layers['apt']));
 layers['apt-clicked']['id'] = 'apt-polygons-clicked';
@@ -96,7 +87,7 @@ layers['apt-clicked']['filter'] = ['==', 'id', ''];
 layers['apt-clicked']['paint']['fill-opacity'] = 0.8;
 
 layers['combined']['id'] = 'combined-polygons';
-layers['combined']['source-layer'] = 'sthlm-lan-combined-pricepolygons';
+layers['combined']['source-layer'] = 'combined-price-polygons-final';
 layers['combined-clicked'] = JSON.parse(JSON.stringify(layers['combined']));
 layers['combined-clicked']['id'] = 'combined-polygons-clicked';
 layers['combined-clicked']['filter'] = ['==', 'id', ''];
@@ -104,13 +95,13 @@ layers['combined-clicked']['paint']['fill-opacity'] = 0.8;
 
 // Contour layers
 layers['apt-contours']['id'] = 'apt-contours';
-layers['apt-contours']['source-layer'] = 'sthlm-lan-apt-contours-60k-hires';
+layers['apt-contours']['source-layer'] = 'apts-sqmprice-contours-final';
 layers['combined-contours']['id'] = 'combined-contours';
-layers['combined-contours']['source-layer'] = 'sthlm-lan-combined-contours-60k-';
+layers['combined-contours']['source-layer'] = 'combined-price-contours-log17';
 
 // Extruded (3D) layers
 layers['apt-extruded']['id'] = 'apt-extruded';
-layers['apt-extruded']['source-layer'] = 'sthlm-lan-sqmprice-polygons-60k';
+layers['apt-extruded']['source-layer'] = 'apts-sqmprice-polygons-final';
 layers['apt-extruded']['paint']['fill-color']['stops'] = sqmprice_color_stops;
 layers['apt-extruded']['paint']['fill-opacity'] = 0.5;
 layers['apt-extruded']['paint']['fill-extrude-height'] = {
@@ -123,7 +114,7 @@ layers['apt-extruded-clicked']['filter'] = ['==', 'id', ''];
 layers['apt-extruded-clicked']['paint']['fill-opacity'] = 0.4;
 
 layers['combined-extruded']['id'] = 'combined-extruded';
-layers['combined-extruded']['source-layer'] = 'sthlm-lan-combined-pricepolygons';
+layers['combined-extruded']['source-layer'] = 'combined-price-polygons-final';
 layers['combined-extruded']['paint']['fill-color']['stops'] = price_color_stops;
 layers['combined-extruded']['paint']['fill-opacity'] = 0.5;
 layers['combined-extruded']['paint']['fill-extrude-height'] = {
@@ -159,7 +150,7 @@ var map = new mapboxgl.Map({
   center: [18.0686, 59.3293],
   zoom: 10,
   minZoom: 9,
-  pitch: extruded_pitch,
+  pitch: flat_pitch,
 });
 
 // Set up map on load
@@ -225,17 +216,11 @@ map.on('load', function () {
     } else {
       map.setFilter(highlight_layer_id, ['==', 'id', '']);
     }
-
-    var center_point = turf.center(feature);
-    var areas = 'https://api.booli.se/areas?lat=' + center_point.geometry.coordinates[1] + '&lng=' + center_point.geometry.coordinates[0] + '&' + Object.keys(auth).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(auth[k])}`).join('&');
-    console.log(areas);
     
     var unit = 'kvadratmeterpris';
     if (state['selection'] == 'combined') unit = 'pris';
-    var popup = new mapboxgl.Popup()
-      .setLngLat(map.unproject(e.point))
-      .setHTML("Genomsnittligt " + unit + ": " + feature.properties.sqmprice.toLocaleString('sv-SE', {style:'currency', currency: 'SEK', maximumFractionDigits: 0}))
-      .addTo(map);
+    var pricestring = "Genomsnittligt " + unit + ": " + feature.properties.sqmprice.toLocaleString('sv-SE', {style:'currency', currency: 'SEK', maximumFractionDigits: 0})
+    document.getElementById('info').innerHTML = pricestring;
   });
 
   // Add zoom and rotation controls to the map.
